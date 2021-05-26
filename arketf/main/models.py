13dff56dd2076e09e44cb61505047f4fbd842b29
brianlulu <juiheng.lu@ryerson.ca>
2021-05-26 22:56:23 +0800

    Add space between navigation bar and body

--- arketf/main/models.py
+++ arketf/main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+
+# class Stock(models.Model):
+#     name = models.CharField(max_length = 100)
+#     ticker = models.CharField(max_length = 20, unique = True)
+#     price = models.DecimalField(max_digits = 20, decimal_places=2)
+#     weight = models.DecimalField(max_digits = 5, decimal_places=2)
+#     market_cap = models.DecimalField(max_digits = 20, decimal_places=2)
+#     ark_own = models.DecimalField(max_digits = 5, decimal_places=2)
+#     market_value = models.DecimalField(max_digits = 20, decimal_places=2)
