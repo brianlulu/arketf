7f41329f64969bd51273fd94717a726678296ce1
brianlulu <juiheng.lu@ryerson.ca>
2021-05-30 14:03:32 +0800

    Add anchor tag to dailytrade page

--- arketf/main/views.py
+++ arketf/main/views.py
@@ -2,27 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Stock, Trade
 
-stocks = [
-    {
-        'name': 'Tesla',
-        'ticker': 'TSLA',
-        'price': 100.87,
-        'market_cap': 98.87,
-    },
-        {
-        'name': 'Microsoft',
-        'ticker': 'MSFT',
-        'price': 187.87,
-        'market_cap': 870.87,
-    },
-]
-
-trade = [
-    {
-        'name': 'ARKK',
-        'holdings': ['Tesla', 'Microsoft']
-    }
-]
 
 def home(request):
 
