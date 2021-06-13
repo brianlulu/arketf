0225b4162cc4c02c7d6c71c900a2d20051474954
brianlulu <juiheng.lu@ryerson.ca>
2021-06-13 22:16:27 +0800

    Add gmail selenium and google api

--- arketf/main/crawlers.py
+++ arketf/main/crawlers.py
@@ -18,27 +18,51 @@ def gmail_login(driver, username, password):
     driver.find_element_by_name("password").send_keys(password)
     driver.find_element_by_xpath('//*[@id="passwordNext"]/div/button').click()
 
-def search(driver, mail_search_key):
+def daily_search(driver, email_search):
 
-    driver.get('https://mail.google.com/')
+    # advance_search = driver.find_element_by_xpath('//*[@id="aso_search_form_anchor"]/button[2]').click()
     
     time.sleep(5)
 
-    search_box = driver.find_element_by_id("mail-search")
-    search_box.find_element_by_tag_name("input").send_keys(mail_search_key)
+    driver.get('https://mail.google.com/mail/?ui=html')
 
-    time.sleep(5)
+    time.sleep(2)
+
+    email_search_input = "from:(" + email_search + ")"
+    search_box = driver.find_element_by_name("q")
+    search_box.send_keys(email_search_input)
+
+    time.sleep(2)
     
-    search_box.find_element_by_xpath('//*[@id="mail-search"]/div/button').click()
+    search_box.find_element_by_xpath('/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[1]/input[2]').click()
+
+    time.sleep(2)
+
+    # email_search_xpath = "//span[@email='" + email_search + "']/ancestor::tr[@role='row']"
+    
+    driver.find_element_by_xpath('/html/body/table[2]/tbody/tr/td[2]/table[1]/tbody/tr/td[2]/form/table[2]/tbody/tr[1]/td[3]/a/span').click()
+
+    time.sleep(2)
+
+    return driver.find_element_by_tag_name('blockquote')
+
+
+
+def previous_content():
 
     time.sleep(5)
 
+    driver.get('https://mail.google.com/mail/u/0?ui=2&ik=68b42d0d8f&view=lg&permmsgid=msg-f:1702363908765375221')
+
+    previous_mail_content = driver.find_elements_by_tag_name('blockquote')
+
+    return previous_mail_content
 
 
 if __name__ == "__main__":
     username = "arketftracker8787"
     password = "fuckingstrongpassword8787!"
-    mail_search_key = "from:ark@ark-funds.com"
+    email_search = "brian33090@gmail.com"
     data_list = []
 
     PATH = "/Users/brianlu/side-projects/ark-etf-website/chromedriver"
@@ -46,29 +70,20 @@ if __name__ == "__main__":
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--incognito")
 
-    driver = webdriver.Chrome(PATH, chrome_options = chrome_options)
+    driver = webdriver.Chrome(PATH, options = chrome_options)
 
     gmail_login(driver, username, password)
 
-    
-    time.sleep(5)
+    daily_mail = daily_search(driver, email_search)
 
-    driver.get('https://mail.google.com/mail/u/0?ui=2&ik=68b42d0d8f&view=lg&permmsgid=msg-f:1702363908765375221')
+    table = daily_mail.find_elements_by_tag_name('tr')
 
-    previous_mail_content = driver.find_elements_by_tag_name('blockquote')
+    for rows in table:
+        row = rows.find_elements_by_tag_name('td')
+        if(row[1].text != "Fund"):
+            for data in row:
+                print(data.text)
+        
 
-    for content in previous_mail_content:
-        table = content.find_elements_by_tag_name('td')
-        data_list.append(table)
-    
-
-    # search(driver, mail_search_key)
-
-    # mail_list_box = driver.find_element_by_xpath('//*[@id="mail-app-component"]/div[1]/div/div[3]/div/div[1]')
-    # print(mail_list_box)
-
-    # driver.quit()
 
     
-    
-# //*[@id="mail-app-component"]/div[1]/div/div[3]/div/div[1]/div[3]/div/div[1]/ul/li[17]/a
