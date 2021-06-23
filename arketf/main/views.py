b44651eb66a8665a8053caa1a50056e843568aef
brianlulu <juiheng.lu@ryerson.ca>
2021-06-23 22:33:50 +0800

    Add pagination to the page

--- arketf/main/views.py
+++ arketf/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import *
 
 def home(request):
@@ -11,92 +12,177 @@ def about(request):
 
 
 def dailytrade(request):
-    
-    trades = {
-        'trades': Trade.objects.all().order_by('-date')
-    }
 
-    return render(request, 'main/dailytrade/dailytrade.html', trades, {'title': 'DailyTrade'})
+    
+    trades = Trade.objects.all().order_by('-date')
+    
+    paginator = Paginator(trades, 30)
+    
+    page = request.GET.get('page')
+    
+    try:
+        page_obj = paginator.page(page)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        page_obj = paginator.page(page.num_pages)
+
+    return render(request,
+        'main/dailytrade/dailytrade.html', 
+        {
+            'Title': 'DailyTrade',
+            'page_obj':page_obj
+        }
+    )
 
 def arkk(request):
     
     fund = Fund.objects.get(ticker = 'ARKK')
 
-    trades = {
-        'trades': Trade.objects.filter(fund = fund).order_by('-date')
-    }
+    trades = Trade.objects.filter(fund = fund).order_by('-date')
 
-    return render(request, 'main/dailytrade/arkk.html', trades, {'title': 'ARKK-DailyTrade'})
+    paginator = Paginator(trades, 30)
+    
+    page = request.GET.get('page')
+    
+    try:
+        page_obj = paginator.page(page)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        page_obj = paginator.page(page.num_pages)
+
+    return render(request, 'main/dailytrade/arkk.html', {'title': 'ARKK-DailyTrade', 'page_obj': page_obj})
 
 
 def arkq(request):
     
     fund = Fund.objects.get(ticker = 'ARKQ')
+    
+    trades = Trade.objects.filter(fund = fund).order_by('-date')
 
-    trades = {
-        'trades': Trade.objects.filter(fund = fund).order_by('-date')
-    }
+    paginator = Paginator(trades, 30)
+    
+    page = request.GET.get('page')
+    
+    try:
+        page_obj = paginator.page(page)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        page_obj = paginator.page(page.num_pages)    
 
-    return render(request, 'main/dailytrade/arkq.html', trades, {'title': 'ARKQ-DailyTrade'})
+    return render(request, 'main/dailytrade/arkq.html', {'title': 'ARKQ-DailyTrade', 'page_obj': page_obj})
 
 
 def arkf(request):
     
     fund = Fund.objects.get(ticker = 'ARKF')
-    trades = {
-        'trades': Trade.objects.filter(fund = fund).order_by('-date')
-    }
+    trades = Trade.objects.filter(fund = fund).order_by('-date')
+    paginator = Paginator(trades, 30)
+    
+    page = request.GET.get('page')
+    
+    try:
+        page_obj = paginator.page(page)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        page_obj = paginator.page(page.num_pages) 
 
-    return render(request, 'main/dailytrade/arkf.html', trades, {'title': 'ARKF-DailyTrade'})
+    return render(request, 'main/dailytrade/arkf.html', {'title': 'ARKF-DailyTrade', 'page_obj': page_obj})
 
 
 def arkg(request):
     
     fund = Fund.objects.get(ticker = 'ARKG')
-    trades = {
-        'trades': Trade.objects.filter(fund = fund).order_by('-date')
-    }
+    
+    trades = Trade.objects.filter(fund = fund).order_by('-date')
+    paginator = Paginator(trades, 30)
+    
+    page = request.GET.get('page')
+    
+    try:
+        page_obj = paginator.page(page)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        page_obj = paginator.page(page.num_pages) 
 
-    return render(request, 'main/dailytrade/arkg.html', trades, {'title': 'ARKG-DailyTrade'})
+    return render(request, 'main/dailytrade/arkg.html', {'title': 'ARKG-DailyTrade', 'page_obj': page_obj})
 
 
 def arkw(request):
     
     fund = Fund.objects.get(ticker = 'ARKW')
-    trades = {
-        'trades': Trade.objects.filter(fund = fund).order_by('-date')
-    }
+    
+    trades = Trade.objects.filter(fund = fund).order_by('-date')
+    paginator = Paginator(trades, 30)
+    
+    page = request.GET.get('page')
+    
+    try:
+        page_obj = paginator.page(page)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        page_obj = paginator.page(page.num_pages) 
 
-    return render(request, 'main/dailytrade/arkw.html', trades, {'title': 'ARKW-DailyTrade'})
+    return render(request, 'main/dailytrade/arkw.html', {'title': 'ARKW-DailyTrade', 'page_obj': page_obj})
 
 
 def arkx(request):
     
     fund = Fund.objects.get(ticker = 'ARKX')
 
-    trades = {
-        'trades': Trade.objects.filter(fund = fund).order_by('-date')
-    }
+    trades = Trade.objects.filter(fund = fund).order_by('-date')
+    paginator = Paginator(trades, 30)
+    
+    page = request.GET.get('page')
+    
+    try:
+        page_obj = paginator.page(page)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        page_obj = paginator.page(page.num_pages) 
 
-    return render(request, 'main/dailytrade/arkx.html', trades, {'title': 'ARKX-DailyTrade'})
+    return render(request, 'main/dailytrade/arkx.html', {'title': 'ARKX-DailyTrade', 'page_obj': page_obj})
 
 
 def prnt(request):
     
     fund = Fund.objects.get(ticker = 'PRNT')
-    trades = {
-        'trades': Trade.objects.filter(fund = fund).order_by('-date')
-    }
+    
+    trades = Trade.objects.filter(fund = fund).order_by('-date')
+    paginator = Paginator(trades, 30)
+    
+    page = request.GET.get('page')
+    
+    try:
+        page_obj = paginator.page(page)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        page_obj = paginator.page(page.num_pages) 
 
-    return render(request, 'main/dailytrade/prnt.html', trades, {'title': 'PRNT-DailyTrade'})
+    return render(request, 'main/dailytrade/prnt.html', {'title': 'PRNT-DailyTrade', 'page_obj': page_obj})
 
 def izrl(request):
     
     fund = Fund.objects.get(ticker = 'IZRL')
 
-    trades = {
-        'trades': Trade.objects.filter(fund = fund).order_by('-date')
-    }
+    trades = Trade.objects.filter(fund = fund).order_by('-date')
+    paginator = Paginator(trades, 30)
+    
+    page = request.GET.get('page')
+    
+    try:
+        page_obj = paginator.page(page)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        page_obj = paginator.page(page.num_pages) 
 
-    return render(request, 'main/dailytrade/izrl.html', trades, {'title': 'IZRL-DailyTrade'})
+    return render(request, 'main/dailytrade/izrl.html', {'title': 'IZRL-DailyTrade', 'page_obj': page_obj})
 
